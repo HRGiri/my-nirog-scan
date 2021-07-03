8e25908f234c5a032b078377da08adc49a1fac65
Himanshu Giri <himanshu.giri08@gmail.com>
2021-07-03 14:21:10 +0530

    Added Charts.java

--- app/src/main/java/com/example/mynirogscan/Charts.java
+++ app/src/main/java/com/example/mynirogscan/Charts.java
@@ -1,4 +1,36 @@
 package com.example.mynirogscan;
 
+import android.graphics.Color;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 public class Charts {
+
+    public void setupLineChart(LineChart lineChart){
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setHighlightPerDragEnabled(true);
+        lineChart.setPinchZoom(true);
+        lineChart.setBackgroundColor(Color.TRANSPARENT);
+        XAxis xAxis = lineChart.getXAxis();
+        lineChart.animateX(1500);
+
+
+        xAxis.setTextSize(11f);
+        xAxis.setTextColor(Color.MAGENTA);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.setTextColor(ColorTemplate.getHoloBlue());
+        yAxis.setAxisMaximum(100f);
+        yAxis.setAxisMinimum(75f);
+        yAxis.setDrawGridLines(true);
+        yAxis.setGranularityEnabled(true);
+
+    }
 }
